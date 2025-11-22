5d0dc266d29ce1596bb8acc21eb1fc1122a82774
Enrique Belarte Luque <ebelarte@redhat.com>
2025-11-22 10:16:54 +0100

    Fix for model download
    Signed-off-by: Enrique Belarte Luque <ebelarte@redhat.com>

--- run-llama.sh
+++ run-llama.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run --rm -it -p 8000:8000 ghcr.io/containers/ramalama run llama3.1:8b-instruct --api --port 8000
+podman run --rm -it -p 8000:8080   quay.io/ramalama/ramalama ramalama serve llama3.1:8b --port 8080
